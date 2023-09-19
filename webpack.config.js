d600b5f55a371a46abd494ee1d0491df4e2d9049
GVVladimir <115398661+GVVladimir@users.noreply.github.com>
2023-09-19 16:05:19 +0300

    цфизфсл

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,35 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+
 module.exports = {
     entry: 'src/main.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        clean: true,
+    },
+    module: {
+        rules: [
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+            {
+                test: /\.(png|svg|jpd|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    plugin: [
+        new CopyPlugin({
+            patterns: [{ from: 'img', to: 'img' }],
+        }),
+    ],
+    Plugins: [
+        new HtmlWebpackPlugin({
+            template: 'index.html',
+        }),
+    ],
 }
